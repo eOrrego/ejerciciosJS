7b2f8a59714c1cfd8e864ccebb4eb05a9015fc85
SteveO <orregoe@gmail.com>
2022-08-31 21:20:03 -0300

    actualizo

--- eje1.js
+++ eje1.js
@@ -585,5 +585,5 @@
 // Arrays
 // Dificultad:  🟢
 // 1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo.
-
+HOla
 
